a4ae85886f5c17cc6e2d47d03b65bc432a945a24
muhammedkurtulus <muhammedkurtulus06@gmail.com>
2023-10-26 15:32:37 +0300

    deployment account

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     hardhat: {
       chainId: 1337,
+      accounts: {
+        count: 30,
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+      },
     },
   },
 };
